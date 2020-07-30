1078da10c59e3062e64052881932a1cba65f295a
vikvas <vikvas6@ya.ru>
2020-07-30 14:51:24 +0300

    Lesson 2 HW

---
+++ CSharp_Lesson2/Task3/Task3.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Task3
+    {
+        //С клавиатуры вводятся числа, пока не будет введен 0. Подсчитать сумму всех нечетных положительных чисел.
+        //Вашченков Виктор
+        static int ReadNumber()
+        {
+            return Convert.ToInt32(Console.ReadLine());
+        }
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            Console.WriteLine("Вводите целые числа, программа остановится, когда будет введён ноль");
+            while (true)
+            {
+                int input = ReadNumber();
+                if (input == 0) break;
+                if (input % 2 == 1 && input > 0) sum += input;
+            }
+
+            Console.WriteLine($"Сумма всех нечетных положительных чисел равна {sum}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CSharp_Lesson2/Task6/Task6.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6
+{
+    class Task6
+    {
+        //*Написать программу подсчета количества «хороших» чисел в диапазоне от 1 до 1 000 000 000.
+        //«Хорошим» называется число, которое делится на сумму своих цифр.
+        //Реализовать подсчёт времени выполнения программы, используя структуру DateTime.
+        //Вашченков Виктор
+        static bool IsGood(int number)
+        {
+            int sum = 0;
+            int cur_num = number;
+            while (cur_num != 0)
+            {
+                sum += cur_num % 10;                
+                cur_num = cur_num / 10;
+            }
+            return number % sum == 0;
+        }
+        static void Main(string[] args)
+        {
+            DateTime start_date = DateTime.Now;
+
+            int cnt = 0;
+            for (int i=1; i<=1000000000; i++)
+            {
+                if (IsGood(i)) cnt++;
+            }
+
+            Console.WriteLine($"Хороших чисел {cnt} найдено за {DateTime.Now - start_date}");
+            Console.ReadKey();
+
+            //Хороших чисел 61574510 найдено за 00:02:15.4247458
+
+        }
+    }
+}
